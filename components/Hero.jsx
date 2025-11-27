fca81efa78ad18996b0b84505369270c8bd6b03a
pouria-honarmand <your_email@example.com>
2025-11-27 15:32:22 -0500

    final

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => {
                             <p>Starts from</p>
                             <p className='text-3xl'>{currency}4.90</p>
                         </div>
-                        <button className='bg-slate-800 text-white text-sm py-2.5 px-7 sm:py-5 sm:px-12 mt-4 sm:mt-10 rounded-md hover:bg-slate-900 hover:scale-103 active:scale-95 transition'>LEARN MORE</button>
+                        {/* <button className='bg-slate-800 text-white text-sm py-2.5 px-7 sm:py-5 sm:px-12 mt-4 sm:mt-10 rounded-md hover:bg-slate-900 hover:scale-103 active:scale-95 transition'>LEARN MORE</button> */}
                     </div>
                     <Image className='sm:absolute bottom-0 right-0 md:right-10 w-full sm:max-w-sm' src={assets.hero_model_img} alt="" />
                 </div>
